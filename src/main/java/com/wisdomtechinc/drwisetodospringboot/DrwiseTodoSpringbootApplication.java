83ee5dda8f2f89ebc8f16306586279d2bcf00dc0
JuliusKanneh <juliuskanneh7@gmail.com>
2024-03-15 12:03:05 +0300

    Initial commit

---
+++ src/main/java/com/wisdomtechinc/drwisetodospringboot/DrwiseTodoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.wisdomtechinc.drwisetodospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrwiseTodoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrwiseTodoSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisdomtechinc/drwisetodospringboot/DrwiseTodoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisdomtechinc.drwisetodospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrwiseTodoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
